19bd146791175a918a4fa4ae0941e5c43559c34a
Braulio Evangelisti <braulioevangelisti@gmail.com>
2015-09-12 15:11:11 -0300

    [Braulio Evangelisti] - 1_7 - Task 7 done: Get response from server.

---
+++ 01-js-intro-and-jquery/07/js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	var alias = '';
+	var focusAlias = function() {	
+		$('.alias').focus(); 
+	};
+	var getResponse = function() {
+		alias = $('.alias').val();
+
+		$.ajax({
+   url: "http://bootcamp.aws.af.cm/welcome/" + alias,
+   type: 'GET',
+   dataType: "json",
+   success: function( data ) {
+   	if(data.error) {
+   		console.log(data.error);
+   	} else {
+   		console.log(data.response);
+   	}
+   }
+		});
+				
+	};
+
+	window.jQuery || document.write('<script src="../js/jquery-2.1.4.min.js"><\/script>');
+
+	$('section').fadeIn(900, focusAlias);
+
+	$('#btnGetResponse').on('click', getResponse);
+	
+});
